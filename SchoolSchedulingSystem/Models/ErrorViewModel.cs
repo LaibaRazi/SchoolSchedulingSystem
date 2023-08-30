b3b8aaad7aec7068289af50097ae2badce029018
Laiba Razi <khanlayba72@gmail.com>
2023-08-31 00:09:47 +0500

    First commit

---
+++ SchoolSchedulingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolSchedulingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
